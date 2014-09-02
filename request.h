7e815c4539359d2d07e72653e6902593aeae0957
Chris Badahdah <a.s.badahdah@gmail.com>
2014-09-01 21:20:21 -0700

    Fixed request struct positions.

--- request.h
+++ request.h
@@ -1,19 +1,23 @@
 #ifndef REQUEST_H
 #define REQUEST_H
 
+// The names of the fields differ from the struct specific in the assignment,
+// but the positions of the elements are the same, so this struct should still
+// work for other teams.
+
 struct request {
-
-  // Client id number
-  int id; // c
-
-  // Request number of client (each request should get incremented by 1)
-  int index;  // r
 
   // Holds client IP address in dotted decimal
   char ip[16]; // client ip
 
   // Name of machine on which client is running
   char name[24];  // m
+
+  // Client id number
+  int id; // c
+
+  // Request number of client (each request should get incremented by 1)
+  int index;  // r
 
   // Incarnation number of client’s machine, for simulating a crash on the
   // clients end
