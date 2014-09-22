8bff427e51b78cbb3315d6e3fe016077d79eb824
Chris Badahdah <a.s.badahdah@gmail.com>
2014-09-21 21:14:26 -0700

    Added back colors.

--- colors.h
+++ colors.h
@@ -2,18 +2,10 @@
 #define COLORS_H
 
 // text color decoration macros
-/*
 #define __blue(str)   "\033[0;34m"str"\033[0;0m"
 #define __yellow(str) "\033[0;33m"str"\033[0;0m"
 #define __green(str)  "\033[0;32m"str"\033[0;0m"
 #define __red(str)    "\033[0;31m"str"\033[0;0m"
 #define __purple(str) "\033[0;35m"str"\033[0;0m"
-*/
-
-#define __blue(str)   str
-#define __yellow(str) str
-#define __green(str)  str
-#define __red(str)    str
-#define __purple(str) str
 
 #endif
